d90e065e24266ec2d08bf3eb498f9e210ebe2ce9
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-01-29 23:20:59 +0100

    cleaned up code

--- main.py
+++ main.py
@@ -7,6 +7,39 @@ import subprocess
 import blessings
 import sys
 term = blessings.Terminal()
+print(term.clear())
+
+fps = 7
+resolution = (10, 100)
+
+class Graph:
+    def __init__(self, resolution, color, n_lines=1, total=None):
+        self.resolution = resolution
+        self.color = color
+        self.total = total
+        self.hist = np.ones((resolution[1], n_lines)) * -1
+
+        self.charmap = np.zeros((resolution[0] + 1, resolution[1] + 1)).astype(str)
+        self.charmap.fill(' ')
+        self.charmap[:, 0] = np.linspace(0, total, resolution[0] + 1).astype(int)
+        self.charmap[:, 1:2] = ''
+        self.charmap[:, 0] = [np.str.ljust(x, 10) for x in self.charmap[:, 0]]
+
+    def step(self, value):
+        self.hist = np.roll(self.hist, -1)
+        self.hist[-1] = list(value) if type(value) is tuple else [value]
+
+    def __str__(self):
+        self.charmap[:, 1:].fill(' ')
+        for i, values in enumerate(self.hist):
+            for x in values:
+                if(x != -1):
+                    symbol = self.color + u'\u2218' + Style.RESET_ALL
+                    y = int(x / self.total * self.resolution[0])
+
+                    self.charmap[y, i + 1] = symbol
+        return '\n'.join(''.join(x) for x in self.charmap.astype(str)[::-1])
+
 
 def gpustats():
     out = subprocess.check_output(['nvidia-smi', '--display=MEMORY', '-q']).decode('utf-8').split('\n')[8:]
@@ -15,74 +48,33 @@ def gpustats():
     used = int(out[2].split()[2])
     return used, total
 
-res = (term.height // 4, term.width - 10)
-pad = 5
+total_ram = psutil.virtual_memory().total / 1024**2
+total_gpu = gpustats()[1]
 
-cpu_hist = np.ones(res[1]) * -1
-ram_hist = np.ones(res[1]) * -1
-gpu_hist = np.ones(res[1]) * -1
-
-total_ram = math.ceil(psutil.virtual_memory().total / 1024**3) * 1024
-total_gpu = math.ceil(gpustats()[1] / 1024) * 1024
-
-def plot(out, color, axis):
-    output = np.zeros((res[0] + 1, res[1] + 1)).astype(str)
-    output[:] = ' '
-
-    output[:, 0] = axis
-
-    for i, x in enumerate(out):
-        if(x != -1):
-            output[int(x * res[0]), i + 1] = color + u'\u2218' + Style.RESET_ALL
-
-    print('\n'.join(''.join(x) for x in output.astype(str)[::-1]))
-
-def show_graphs():
-    with term.location(0, 1):
-        print(f'{Back.GREEN}CPU Usage in Percent:{Style.RESET_ALL}')
-
-        cpu_axis = np.array([str(x * int((100 / res[0]))) for x in range(res[0] + 1)])
-        cpu_axis[1::2] = ''
-        cpu_axis = [np.str.ljust(x, 10) for x in cpu_axis]
-
-        plot(cpu_hist, Fore.GREEN, cpu_axis)
-    with term.location(0, res[0] + pad):
-        print(f'{Back.BLUE}RAM Usage in MiB:{Style.RESET_ALL}')
-        ram_axis = np.arange(0, total_ram, total_ram // res[0]).astype(str)
-        ram_axis[1::2] = ''
-        ram_axis = [np.str.ljust(x, 10) for x in ram_axis]
-
-        plot(ram_hist, Fore.BLUE, ram_axis)
-    with term.location(0, (res[0] + pad) * 2):
-        print(f'{Back.RED}GPU RAM Usage in MiB:{Style.RESET_ALL}')
-        gpu_axis = np.arange(0, total_gpu, total_gpu // res[0]).astype(str)
-        gpu_axis[1::2] = ''
-        gpu_axis = [np.str.ljust(x, 10) for x in gpu_axis]
-
-        plot(gpu_hist, Fore.RED, gpu_axis)
-
-prevdiskio = psutil.disk_io_counters()
 if __name__ == '__main__':
     try:
-        while True:
-            cpu_hist = np.roll(cpu_hist, -1)
-            cpu_hist[-1] = psutil.cpu_percent() / 100
+        cpu_graph = Graph(resolution, Fore.GREEN, total=100)
+        gpu_graph = Graph(resolution, Fore.RED, total=total_gpu)
+        ram_graph = Graph(resolution, Fore.YELLOW, total=total_ram)
 
-            ram_hist = np.roll(ram_hist, -1)
-            ram_hist[-1] = (psutil.virtual_memory().used / 1024**2) / total_ram
+        with term.hidden_cursor():
+            while True:
+                print(term.move(0, 1))
 
-            gpu_hist = np.roll(gpu_hist, -1)
-            gpu_hist[-1] = gpustats()[0] / total_gpu
+                print(f'{Back.GREEN}CPU Usage in Percent:{Style.RESET_ALL}')
+                cpu_graph.step(psutil.cpu_percent())
+                print(cpu_graph)
 
-            diskio = psutil.disk_io_counters()
-            read_bytes = diskio.read_bytes - prevdiskio.read_bytes
-            write_bytes = diskio.write_bytes - prevdiskio.write_bytes
-            prevdiskio = diskio
+                print(f'{Back.RED}GPU Usage in MiB:{Style.RESET_ALL}')
+                gpu_graph.step(gpustats()[0])
+                print(gpu_graph)
 
+                print(f'{Back.YELLOW}RAM Usage in MiB:{Style.RESET_ALL}')
+                ram_graph.step(psutil.virtual_memory().used / 1024**2)
+                print(ram_graph)
 
-            show_graphs()
-            time.sleep(1/10)
+                time.sleep(1/fps)
     except KeyboardInterrupt:
         print('Stopping..')
-        print(term.clear())
+        # print(term.clear())
         sys.exit(0)
