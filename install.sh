22d4ba6f6862a25e6d1db83a841e35a69f8c2668
bminixhofer <bminixhofer@gmail.com>
2018-01-30 16:13:52 +0100

    provide options for optional packages in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-sudo apt install nvidia-smi sysstat
 pip install -r requirements.txt
+
+echo ""
+read -n1 -p "Install nvidia-smi to monitor GPU RAM? [y,n]" gpu
+case $gpu in
+  y|Y) sudo apt install nvidia-smi ;;
+  *) echo "" ;;
+esac
+
+echo ""
+read -n1 -p "Install sysstat to monitor disk read / write speed? [y,n]" io
+
+case $io in
+  y|Y) sudo apt install sysstat ;;
+  *) echo "" ;;
+esac
