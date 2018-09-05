b9c5195ae2709eb5b03d806488fdf916c227dd9b
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-09-05 22:19:42 +0200

    switched sysstats -> psutil for more accurate measurement and less external deps

--- install.sh
+++ install.sh
@@ -7,11 +7,3 @@ case $gpu in
   y|Y) sudo apt install nvidia-smi ;;
   *) echo "" ;;
 esac
-
-echo ""
-read -n1 -p "Install sysstat to monitor disk read / write speed? [y,n]" io
-
-case $io in
-  y|Y) sudo apt install sysstat ;;
-  *) echo "" ;;
-esac
--- permon/backend.py
+++ permon/backend.py
@@ -1,8 +1,7 @@
 import psutil
-import threading
-from subprocess import Popen, PIPE
 from permon.classes import Stat
 import os
+import time
 
 
 @Stat.windows
@@ -75,102 +74,54 @@ class RAMStat(Stat):
 #         return self._maximum
 
 
-class IoReader():
-    instance = None
-    n_instances = 0
-
-    class __IoReader:
-        def __init__(self):
-            self._read = 0
-            self._write = 0
-            self._thread = threading.Thread(target=self._io_process)
-            self._thread.start()
-
-            self._thread_is_stopped = False
-            self._stop_thread = False
-
-        def _io_process(self):
-            p = Popen('iostat -m 1 -g ALL -H'.split(),
-                      stdout=PIPE, stderr=PIPE)
-            for line in p.stdout:
-                line = line.decode('utf-8')
-                if line.strip().startswith('ALL'):
-                    stat_str = line.split()[2:4]
-                    # replace , with . to be convert the string to a number
-                    read_write = [float(x.replace(',', '.')) for x in stat_str]
-                    self._read = read_write[0]
-                    self._write = read_write[1]
-
-                if self._stop_thread:
-                    self._thread_is_stopped = True
-                    break
-
-        def stop_thread(self):
-            self._stop_thread = True
-            while not self._thread_is_stopped:
-                continue
-
-        @property
-        def read(self):
-            return self._read
-
-        @property
-        def write(self):
-            return self._write
-
-    def __del__(self):
-        IoReader.n_instances -= 1
-        # delete the instance when the last singleton wrapper is deleted
-        if IoReader.n_instances == 0:
-            IoReader.instance.stop_thread()
-            IoReader.instance = None
-
-    def __init__(self):
-        IoReader.n_instances += 1
-        if not self.instance:
-            IoReader.instance = self.__IoReader()
-
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
-
-
+@Stat.windows
 @Stat.linux
 class ReadStat(Stat):
     name = 'Disk Read Speed in MiB / s'
     tag = 'read_speed'
 
-    def __init__(self):
-        self.reader = IoReader()
+    def __init__(self, fps=10):
+        self.cache = []
+        self.start_bytes = psutil.disk_io_counters().read_bytes
 
     def get_stat(self):
-        return self.reader.read
+        stat = psutil.disk_io_counters().read_bytes - self.start_bytes
+        current_time = time.time()
+        self.cache.append((stat, current_time))
+        self.cache = [(x, t) for x, t in self.cache if current_time - t <= 1]
+
+        return float(self.cache[-1][0] - self.cache[0][0]) / 1024**2
 
     @property
     def minimum(self):
-        return 0
+        return None
 
     @property
     def maximum(self):
         return None
 
-    def destruct(self):
-        del self.reader
 
-
+@Stat.windows
 @Stat.linux
 class WriteStat(Stat):
     name = 'Disk Write Speed in MiB / s'
     tag = 'write_speed'
 
-    def __init__(self):
-        self.reader = IoReader()
+    def __init__(self, fps=10):
+        self.cache = []
+        self.start_bytes = psutil.disk_io_counters().write_bytes
 
     def get_stat(self):
-        return self.reader.write
+        stat = psutil.disk_io_counters().write_bytes - self.start_bytes
+        current_time = time.time()
+        self.cache.append((stat, current_time))
+        self.cache = [(x, t) for x, t in self.cache if current_time - t <= 1]
+
+        return float(self.cache[-1][0] - self.cache[0][0]) / 1024**2
 
     @property
     def minimum(self):
-        return 0
+        return None
 
     @property
     def maximum(self):
