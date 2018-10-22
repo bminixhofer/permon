7ac8b7afe882987433344407e5a98f253a82ebcd
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-10-22 18:19:24 +0200

    made chart axis font scale with number of monitors and app size

--- permon/frontend/native.py
+++ permon/frontend/native.py
@@ -97,11 +97,11 @@ class SettingsWidget(QtWidgets.QWidget):
 
 class NativeMonitor(Monitor):
     def __init__(self, stat, buffer_size, fps, color,
-                 app, fontsize, thickness):
+                 app, thickness, fontsize):
         super(NativeMonitor, self).__init__(stat, buffer_size,
                                             fps, color, app)
-        self.fontsize = fontsize
         self.thickness = thickness
+        self.fontsize = fontsize
         self.widget = QtWidgets.QWidget()
 
         # the widget consists of a title and the chart
@@ -112,12 +112,6 @@ class NativeMonitor(Monitor):
 
         self.widget.chart = QtCharts.QChart()
 
-        # use a QCategoryAxis again for more customizability
-        self.widget.axisY = QtCharts.QCategoryAxis()
-        self.widget.axisY.setLabelsPosition(
-            QtCharts.QCategoryAxis.AxisLabelsPositionOnValue
-        )
-
         self._create_line_series()
         self._create_right_axis()
 
@@ -127,10 +121,11 @@ class NativeMonitor(Monitor):
         self.widget.chart.layout().setContentsMargins(0, 0, 0, 0)
 
         # create the title label and add it to the layout
-        layout.addWidget(self._create_header(fontsize))
+        layout.addWidget(self._create_header())
 
         # create a view for the chart and add it to the layout
         self.widget.chartView = QtCharts.QChartView(self.widget.chart)
+        self.widget.chartView.setContentsMargins(0, 0, 0, 0)
         self.widget.chartView.setRenderHint(QtGui.QPainter.Antialiasing)
         layout.addWidget(self.widget.chartView)
 
@@ -140,27 +135,29 @@ class NativeMonitor(Monitor):
         timer.timeout.connect(self.update)
 
     def _create_right_axis(self):
-        top_axis = QtCharts.QCategoryAxis()
-        top_axis.setGridLineVisible(False)
-
-        font = QtGui.QFont()
-        font.setPixelSize(8)
-        top_axis.setLabelsFont(font)
-        top_axis.setLinePen(self.line_pen)
+        axisY = QtCharts.QCategoryAxis()
+        axisY.setGridLineVisible(False)
+        axisY.setLinePen(self.line_pen)
 
         if not self.stat.has_contributor_breakdown:
-            top_axis.setLineVisible(False)
+            axisY.setLineVisible(False)
 
-        self.widget.chart.addAxis(top_axis, Qt.AlignRight)
-        self.line_series.attachAxis(top_axis)
+        self.widget.chart.addAxis(axisY, Qt.AlignRight)
+        self.line_series.attachAxis(axisY)
 
-        self.top_axis = top_axis
+        self.right_axis = axisY
 
     def _create_line_series(self):
         # a QValueAxis has tick labels per default
         # so we use a QCategoryAxis
         axisX = QtCharts.QCategoryAxis()
         axisX.setRange(0, self.buffer_size)
+
+        # use a QCategoryAxis again for more customizability
+        axisY = QtCharts.QCategoryAxis()
+        axisY.setLabelsPosition(
+            QtCharts.QCategoryAxis.AxisLabelsPositionOnValue
+        )
 
         # create series
         series = QtCharts.QLineSeries()
@@ -169,21 +166,23 @@ class NativeMonitor(Monitor):
 
         self.widget.chart.addSeries(series)
         self.widget.chart.setAxisX(axisX, series)
-        self.widget.chart.setAxisY(self.widget.axisY, series)
+        self.widget.chart.setAxisY(axisY, series)
 
         self.buffer = [QtCore.QPointF(x, 0) for x in range(self.buffer_size)]
         series.append(self.buffer)
 
+        self.x_axis = axisX
+        self.left_axis = axisY
         self.line_pen = pen
         self.line_series = series
 
     def _set_yrange(self, minimum, maximum, n_labels=5):
-        axis = self.widget.axisY
+        axis = self.left_axis
         if minimum == axis.min() and maximum == axis.max():
             return
 
         axis.setRange(minimum, maximum)
-        self.top_axis.setRange(minimum, maximum)
+        self.right_axis.setRange(minimum, maximum)
 
         for s in axis.categoriesLabels():
             axis.remove(s)
@@ -194,17 +193,26 @@ class NativeMonitor(Monitor):
         for value, label in zip(axis_values, axis_labels):
             # qt strips spaces from labels, to make them have the same size
             # we have to pad them with non-breaking spaces (U+00A0)
-            axis.append(label.rjust(10, u'\u00A0'), value)
+            axis.append(label, value)
 
-    def _create_header(self, fontsize):
+    def _create_header(self):
         font = QtGui.QFont()
-        font.setPixelSize(fontsize)
+        font.setPixelSize(14)
         font.setBold(True)
 
         title_label = QtWidgets.QLabel(self.stat.name)
         title_label.setFont(font)
 
         return title_label
+
+    def adjust_fonts(self, app_height, app_width, n_monitors):
+        font = QtGui.QFont()
+        real_fontsize = self.fontsize / n_monitors * app_height / 1000
+        real_fontsize = min(real_fontsize, 16)
+        font.setPixelSize(real_fontsize)
+
+        self.right_axis.setLabelsFont(font)
+        self.left_axis.setLabelsFont(font)
 
     def update(self):
         # every frame, we remove the last point of the history and
@@ -215,16 +223,16 @@ class NativeMonitor(Monitor):
             value = self.stat.get_stat()
             contributors = {}
 
-        for s in self.top_axis.categoriesLabels():
-                self.top_axis.remove(s)
+        for s in self.right_axis.categoriesLabels():
+                self.right_axis.remove(s)
 
         agg = 0
         for i, x in enumerate(contributors):
             label = utils.format_bar_label(x[0])
 
             agg += x[1]
-            self.top_axis.append(u'\u00A0' * 2 + label, agg)
-        self.top_axis.append(u'\u00A0' * 40, math.inf)
+            self.right_axis.append(u'\u00A0' * 2 + label, agg)
+        self.right_axis.append(u'\u00A0' * 40, math.inf)
 
         new_point = QtCore.QPointF(self.buffer_size, value)
         self.buffer.append(new_point)
@@ -267,7 +275,7 @@ class NativeApp(MonitorApp):
     # QApplication is a global singleton. It can only ever be instantiated once
     qapp = None
 
-    def __init__(self, stats, colors, buffer_size, fps, fontsize=14,
+    def __init__(self, stats, colors, buffer_size, fps, fontsize=50,
                  line_thickness=2):
         super(NativeApp, self).__init__(stats, colors, buffer_size, fps)
 
@@ -314,6 +322,12 @@ class NativeApp(MonitorApp):
         size = availableGeometry.height() * 3 / 4
         self.window.resize(size, size)
 
+        def resize_event(event):
+            QtWidgets.QMainWindow.resizeEvent(self.window, event)
+            self.adjust_fonts(event.size().height(), event.size().width())
+
+        self.window.resizeEvent = resize_event
+
         # create the main widget and add monitors to it
         self._main = QtWidgets.QStackedWidget()
         self._settings_page = SettingsWidget(self._main, self.stats)
@@ -356,6 +370,11 @@ class NativeApp(MonitorApp):
     def paint(self):
         pass
 
+    def adjust_fonts(self, app_height, app_width):
+        n_monitors = len(self.monitors)
+        for monitor in self.monitors:
+            monitor.adjust_fonts(app_height, app_width, n_monitors)
+
     def adjust_monitors(self):
         displayed_stats = []
         removed_monitors = []
@@ -385,6 +404,11 @@ class NativeApp(MonitorApp):
         # keep all charts equally large
         for i in range(self._monitor_page.layout().count()):
             self._monitor_page.layout().setStretch(i, 1)
+
+        # adjust axis font sizes of all monitors
+        availableGeometry = self.qapp.desktop().availableGeometry(self.window)
+        self.adjust_fonts(availableGeometry.height(),
+                          availableGeometry.width())
 
         if logging.getLogger().isEnabledFor(logging.INFO):
             displayed = [stat.tag for stat in displayed_stats] or 'No Monitors'
