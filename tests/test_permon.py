4bdf6296c0adab4054bd28c6b481a2390a2edb55
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-11-26 11:19:56 +0100

    added tests for config and security

--- tests/test_permon.py
+++ tests/test_permon.py
@@ -2,10 +2,13 @@ import sys
 import pytest
 import warnings
 import permon
+import time
+import random
+import os
+import secrets
 from permon.frontend import native, terminal, browser
-import permon.backend as backend
 from permon.backend import Stat
-from permon import exceptions
+from permon import exceptions, backend, config, security
 
 FPS = 10
 warnings.filterwarnings("ignore")
@@ -68,3 +71,36 @@ def test_init(app, arguments, mocker):
 
     permon.main()
     patched_init.assert_called_once()
+
+
+def test_constant_secret_key():
+    if os.path.exists(security.secret_path):
+        os.remove(security.secret_path)
+    key = security.get_secret_key()
+    time.sleep(random.random())
+    assert key == security.get_secret_key()
+
+
+def test_constant_encryption():
+    password = 'test123'
+    pw_hash = security.encrypt_password(password)
+    time.sleep(random.random())
+    assert pw_hash == security.encrypt_password(password)
+
+
+def test_set_config():
+    config.reset_config()
+    previous_stats = config.get_config()['stats'].copy()
+
+    config.set_config({
+        'stats': previous_stats + ['test']
+    })
+    assert len(config.get_config()['stats']) == len(previous_stats) + 1
+    config.reset_config()
+
+
+def test_set_invalid_config():
+    with pytest.raises(AssertionError):
+        config.set_config({
+            secrets.token_hex(10): 1
+        })
