b9763981976d767e0dd10f3a8777bb6f57bcf194
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-11-28 22:40:43 +0100

    added first design of docs

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.image-changer').forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+        document.querySelector(elem.dataset.target).src = elem.dataset.url;
+        elem.parentElement.querySelectorAll('.image-changer').forEach((elem) => {
+            elem.classList.remove('selected');
+        });
+        elem.classList.add('selected');
+    });
+});
