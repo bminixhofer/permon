7d8136861381618b50c92a8b791f33a6c38d6098
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-01 12:02:23 +0100

    finished content of landing page

--- docs/index.js
+++ docs/index.js
@@ -2,8 +2,15 @@ hljs.initHighlightingOnLoad();
 
 document.querySelectorAll('.image-changer').forEach((elem) => {
     elem.addEventListener('click', (event) => {
-        document.querySelector(elem.dataset.target).src = elem.dataset.url;
-        elem.parentElement.querySelectorAll('.image-changer').forEach((elem) => {
+        const parent = document.querySelector(elem.dataset.target)
+        const target = parent.children[parseInt(elem.dataset.child)];
+
+        Array.from(parent.children).forEach((elem) => {
+            elem.style.opacity = 0;
+        })
+        target.style.opacity = 1;
+
+        parent.parentElement.querySelectorAll('.image-changer').forEach((elem) => {
             elem.classList.remove('selected');
         });
         elem.classList.add('selected');
