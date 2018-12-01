098190d6e96804b1de12296ca9f96e5bde188564
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-01 17:58:19 +0100

    fixed videos and first displayed screenshot

--- docs/index.js
+++ docs/index.js
@@ -1,18 +1,25 @@
 hljs.initHighlightingOnLoad();
 
 document.querySelectorAll('.image-changer').forEach((elem) => {
+    if(elem.classList.contains('selected')) {
+        selectElement(elem);
+    }
     elem.addEventListener('click', (event) => {
-        const parent = document.querySelector(elem.dataset.target)
-        const target = parent.children[parseInt(elem.dataset.child)];
-
-        Array.from(parent.children).forEach((elem) => {
-            elem.style.opacity = 0;
-        })
-        target.style.opacity = 1;
-
-        parent.parentElement.querySelectorAll('.image-changer').forEach((elem) => {
-            elem.classList.remove('selected');
-        });
-        elem.classList.add('selected');
+        selectElement(elem);
     });
 });
+
+function selectElement(elem) {
+    const parent = document.querySelector(elem.dataset.target)
+    const target = parent.children[parseInt(elem.dataset.child)];
+
+    Array.from(parent.children).forEach((elem) => {
+        elem.style.opacity = 0;
+    })
+    target.style.opacity = 1;
+
+    parent.parentElement.querySelectorAll('.image-changer').forEach((elem) => {
+        elem.classList.remove('selected');
+    });
+    elem.classList.add('selected');
+}
