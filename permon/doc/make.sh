c016660927c7868d953c7db3591add4d7bd24c31
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-04 16:30:53 +0100

    lint and build docs in circleci

--- permon/doc/make.sh
+++ permon/doc/make.sh
@@ -2,5 +2,5 @@ SOURCEDIR=`dirname "$0"`
 TARGETDIR=${SOURCEDIR}/../../docs
 # clean the target directory
 rm -rf ${TARGETDIR}/*
-npm --prefix ${SOURCEDIR} run build
-sphinx-build -b dirhtml ${SOURCEDIR} ${TARGETDIR}
+npm --prefix ${SOURCEDIR} run build || { echo 'npm build failed' ; exit 1; }
+sphinx-build -b dirhtml ${SOURCEDIR} ${TARGETDIR} || { echo 'sphinx-build failed' ; exit 1; }
