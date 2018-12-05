0ef4de7f6d0b32a40a3cd044477a715f08c52939
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-05 09:50:18 +0100

    fixed git hook to work on older git versions

--- bin/init.sh
+++ bin/init.sh
@@ -3,4 +3,4 @@ set -e
 PROJECT_ROOT=`dirname "$0"`/../
 npm --prefix ${PROJECT_ROOT}/permon/doc/ install
 npm --prefix ${PROJECT_ROOT}/permon/frontend/browser/ install
-git config core.hooksPath .githooks
+cp ${PROJECT_ROOT}/.githooks/* ${PROJECT_ROOT}/.git/hooks/
