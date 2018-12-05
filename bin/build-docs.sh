44e1c18026686c556d6dbc84b7e185c9b089135d
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-05 12:12:40 +0100

    fixed root url when publishing docs to github

--- bin/build-docs.sh
+++ bin/build-docs.sh
@@ -6,4 +6,4 @@ TARGETDIR=${PROJECT_ROOT}/docs/
 # clean the target directory
 rm -rf ${TARGETDIR}/*
 npm --prefix ${SOURCEDIR} run build
-sphinx-build -b dirhtml ${SOURCEDIR} ${TARGETDIR}
+sphinx-build -b dirhtml ${SOURCEDIR} ${TARGETDIR} $1
