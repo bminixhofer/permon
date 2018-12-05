f33162d298a3960eeea9822ce2bd11f2cceef363
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-05 12:58:03 +0100

    fixed broken links in docs

--- permon/doc/conf.py
+++ permon/doc/conf.py
@@ -94,10 +94,7 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'page_width': '1300px',
-    'sidebar_width': '400px'
-}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
