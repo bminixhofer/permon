bd928f5d5a6b209a19276377edcf89e914913f18
Benjamin Minixhofer <bminixhofer@gmail.com>
2018-12-05 09:04:45 +0100

    fixed CPUTempStat availability on windows

--- permon/backend/stats/core.py
+++ permon/backend/stats/core.py
@@ -1,108 +1,12 @@
 import time
 import re
+import os
 import threading
 from collections import defaultdict
 import subprocess
 import psutil
 from permon.backend import Stat
 from permon import exceptions
-
-
-class ProcessTracker():
-    """
-    Singleton wrapper for tracking process information like cpu and
-    RAM usage of each process. Needed by core.ram_usage and core.cpu_usage
-    for their contributor breakdown.
-    """
-    instance = None
-    n_wrapper_instances = 0
-
-    def __init__(self):
-        if not self.instance:
-            ProcessTracker.instance = self._ProcessTracker()
-        ProcessTracker.n_wrapper_instances += 1
-
-    def __del__(self):
-        ProcessTracker.n_wrapper_instances -= 1
-        if self.n_wrapper_instances == 0:
-            self.instance._stop = True
-            while not self.instance._stopped:
-                continue
-
-            ProcessTracker.instance = None
-
-    def __getattr__(self, attr):
-        return getattr(self.instance, attr)
-
-    class _ProcessTracker():
-        """Class actually tracking the processes."""
-        def __init__(self):
-            self._stop = False
-            self._stopped = False
-            self.processes = {}
-            self.contributors = defaultdict(lambda: [])
-
-            # start a thread for continuously reading all processes
-            self._thread = threading.Thread(target=self._read_processes)
-            self._thread.start()
-
-        def _read_processes(self):
-            # while the tracker is running, read the cpu and ram usage
-            # of all running processes
-            while not self._stop:
-                iterator = psutil.process_iter()
-                _processes = {}
-
-                for proc in iterator:
-                    name = re.split(r'[\W\s]+', proc.name())[0]
-                    if name not in _processes:
-                        _processes[name] = {
-                            'cpu': proc.cpu_percent(),
-                            'ram': proc.memory_info().vms
-                        }
-                    else:
-                        _processes[name]['cpu'] += proc.cpu_percent()
-                        _processes[name]['ram'] += proc.memory_info().vms
-
-                    # sleep for a short time to allow the UI thread to continue
-                    time.sleep(0.02)
-
-                # used_memory = psutil.virtual_memory().used / 1000**2
-                self.contributors['cpu'] = self.get_contributors('cpu')
-                self.contributors['ram'] = self.get_contributors('ram')
-                self.processes = _processes
-
-            self._stopped = True
-
-        def get_contributors(self, tag, n=5, adapt_to=None):
-            """
-            Get the top n contributors to a tag
-            where `tag` is either `cpu` or `ram`.
-            If adapt_to is not None, scale the contributors such that
-            their sum is equal to adapt_to.
-            """
-            processes = self.processes.items()
-            contributors = sorted(processes, key=lambda proc: proc[1][tag],
-                                  reverse=True)
-            contributors = [[key, value[tag]] for key, value in contributors]
-
-            if adapt_to is not None:
-                value_sum = max(sum(x[1] for x in contributors), 1e-6)
-                # scale the contributors so that they amount to adapt_to
-                for i, (_, value) in enumerate(contributors):
-                    contributors[i][1] = value / value_sum * adapt_to
-                # combine the contributors that are smaller than the top n
-                # into one
-                remainder = adapt_to - sum(x[1] for x in contributors[:n-1])
-                contributors.insert(0, ['other', remainder])
-            return contributors[:n]
-
-    def delete_instance(self):
-        self.instance._stop = True
-        while not self._stopped:
-            continue
-
-        self.instance = None
 
 
 class CPUStat(Stat):
@@ -334,6 +238,9 @@ class CPUTempStat(Stat):
 
     @classmethod
     def check_availability(cls):
+        if os.name == 'nt':
+            raise exceptions.StatNotAvailableError(
+                'psutil.sensors_temperatures() is not available on windows.')
         # psutil.sensors_temperatures returns a dictionary
         # consisting of multiple temperature measurements
         # `coretemp` is the only one currently considered
@@ -368,3 +275,100 @@ class CPUTempStat(Stat):
     @property
     def maximum(self):
         return self._maximum
+
+
+class ProcessTracker():
+    """
+    Singleton wrapper for tracking process information like cpu and
+    RAM usage of each process. Needed by core.ram_usage and core.cpu_usage
+    for their contributor breakdown.
+    """
+    instance = None
+    n_wrapper_instances = 0
+
+    def __init__(self):
+        if not self.instance:
+            ProcessTracker.instance = self._ProcessTracker()
+        ProcessTracker.n_wrapper_instances += 1
+
+    def __del__(self):
+        ProcessTracker.n_wrapper_instances -= 1
+        if self.n_wrapper_instances == 0:
+            self.instance._stop = True
+            while not self.instance._stopped:
+                continue
+
+            ProcessTracker.instance = None
+
+    def __getattr__(self, attr):
+        return getattr(self.instance, attr)
+
+    class _ProcessTracker():
+        """Class actually tracking the processes."""
+        def __init__(self):
+            self._stop = False
+            self._stopped = False
+            self.processes = {}
+            self.contributors = defaultdict(lambda: [])
+
+            # start a thread for continuously reading all processes
+            self._thread = threading.Thread(target=self._read_processes)
+            self._thread.start()
+
+        def _read_processes(self):
+            # while the tracker is running, read the cpu and ram usage
+            # of all running processes
+            while not self._stop:
+                iterator = psutil.process_iter()
+                _processes = {}
+
+                for proc in iterator:
+                    name = re.split(r'[\W\s]+', proc.name())[0]
+                    if name not in _processes:
+                        _processes[name] = {
+                            'cpu': proc.cpu_percent(),
+                            'ram': proc.memory_info().vms
+                        }
+                    else:
+                        _processes[name]['cpu'] += proc.cpu_percent()
+                        _processes[name]['ram'] += proc.memory_info().vms
+
+                    # sleep for a short time to allow the UI thread to continue
+                    time.sleep(0.02)
+
+                # used_memory = psutil.virtual_memory().used / 1000**2
+                self.contributors['cpu'] = self.get_contributors('cpu')
+                self.contributors['ram'] = self.get_contributors('ram')
+                self.processes = _processes
+
+            self._stopped = True
+
+        def get_contributors(self, tag, n=5, adapt_to=None):
+            """
+            Get the top n contributors to a tag
+            where `tag` is either `cpu` or `ram`.
+            If adapt_to is not None, scale the contributors such that
+            their sum is equal to adapt_to.
+            """
+            processes = self.processes.items()
+            contributors = sorted(processes, key=lambda proc: proc[1][tag],
+                                  reverse=True)
+            contributors = [[key, value[tag]] for key, value in contributors]
+
+            if adapt_to is not None:
+                value_sum = max(sum(x[1] for x in contributors), 1e-6)
+                # scale the contributors so that they amount to adapt_to
+                for i, (_, value) in enumerate(contributors):
+                    contributors[i][1] = value / value_sum * adapt_to
+                # combine the contributors that are smaller than the top n
+                # into one
+                remainder = adapt_to - sum(x[1] for x in contributors[:n-1])
+                contributors.insert(0, ['other', remainder])
+            return contributors[:n]
+
+    def delete_instance(self):
+        self.instance._stop = True
+        while not self._stopped:
+            continue
+
+        self.instance = None
